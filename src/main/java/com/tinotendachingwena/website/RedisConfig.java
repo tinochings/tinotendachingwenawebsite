248554fd85be856043a82be0be51c0b22301c971
Tino <tinotendachings@gmail.com>
2023-09-17 18:51:34 +0200

    Updating RedisConfig to disable TLS

--- src/main/java/com/tinotendachingwena/website/RedisConfig.java
+++ src/main/java/com/tinotendachingwena/website/RedisConfig.java
@@ -6,6 +6,7 @@ import io.github.bucket4j.distributed.proxy.ProxyManager;
 import io.github.bucket4j.grid.jcache.JCacheProxyManager;
 import org.redisson.config.Config;
 import org.redisson.jcache.configuration.RedissonConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -16,10 +17,17 @@ import javax.cache.Caching;
 @Configuration
 public class RedisConfig {
     @Bean
+    public LettuceClientConfigurationBuilderCustomizer lettuceClientConfigurationBuilderCustomizer() {
+        return clientConfigurationBuilder -> {
+            if (clientConfigurationBuilder.build().isUseSsl()) {
+                clientConfigurationBuilder.useSsl().disablePeerVerification();
+            }
+        };
+    }
+    @Bean
     public Config config() {
         Config config = new Config();
-        config.useClusterServers().setPassword("p6f2f323da02ad7f3c0f0ffad78c507c2c571df99480cd18bac353a9a2b3bb0d0").addNodeAddress("rediss://:p6f2f323da02ad7f3c0f0ffad78c507c2c571df99480cd18bac353a9a2b3bb0d" +
-                "0@ec2-63-33-139-127.eu-west-1.compute.amazonaws.com:16710");
+        config.useSingleServer().setPassword("p6f2f323da02ad7f3c0f0ffad78c507c2c571df99480cd18bac353a9a2b3bb0d0").setAddress("rediss://:p6f2f323da02ad7f3c0f0ffad78c507c2c571df99480cd18bac353a9a2b3bb0d0@ec2-63-33-139-127.eu-west-1.compute.amazonaws.com:16710");
         return config;
     }
 
