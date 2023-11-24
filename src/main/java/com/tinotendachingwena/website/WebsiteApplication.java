ac9b26e550147957eea4575d01a91f8cd873cb71
Tino <tinotendachings@gmail.com>
2023-11-24 22:37:40 +0200

    Enabled caching

--- src/main/java/com/tinotendachingwena/website/WebsiteApplication.java
+++ src/main/java/com/tinotendachingwena/website/WebsiteApplication.java
@@ -3,9 +3,11 @@ package com.tinotendachingwena.website;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableEncryptableProperties
+@EnableCaching
 public class WebsiteApplication {
 
 	public static void main(String[] args) {
